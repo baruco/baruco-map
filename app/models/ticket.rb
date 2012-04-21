292cdde5932407e38f75edcda9212ce09759c08d
Josep Jaume <josepjaume@gmail.com>
2012-04-21 19:58:52 +0200

    Remove city from geocoding

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,13 +1,24 @@
 class Ticket < ActiveRecord::Base
 
-  geocoded_by :partial_address
+  geocoded_by :partial_address do |obj, results|
+    if geo = results.first
+      obj.city = geo.city
+    end
+  end
+
   before_save :geocode
 
-  def partial_address
+  def address
     [city, zip, country].join(', ')
   end
 
   def geocode
-    super if !city.blank? && !country.blank? && !zip.blank?
+    results = Geocoder.search(address)
+    if geo = results.first
+      results = Geocoder.search("#{geo.city}, #{geo.country}")
+      result = results.first
+      self.longitude = result.longitude
+      self.latitude = result.latitude
+    end
   end
 end
